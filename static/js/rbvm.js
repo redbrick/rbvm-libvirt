2af6e85a5edf1209160189db8871ee98d910e28c
Andrew Martin <werdz@redbrick.dcu.ie>
2011-12-29 20:37:16 +0000

    Commented some stuff, fixed login detection

--- static/js/rbvm.js
+++ static/js/rbvm.js
@@ -1,4 +1,18 @@
+/**
+ * Navigation thingy.
+ *
+ * When the page URL changes (the bit after the #), one of these
+ * is called, depending on the URL. 
+ *
+ * Each function needs to take an event parameter, which is an
+ * object with the following params:
+ * .parameters  URL parameters dictionary extracted from the query string (i.e. ?param1=value&param2=value is returned as {'param1':'value', 'param2':'value'})
+ * .path        The URL path (/, or /home, or /foo/bar, etc)
+ * .pathNames   Path component names ('', or 'home', or 'foo, bar', etc)
+ * .queryString The raw query string
+ */
 var nav = {
+    
     '/' : function( event ) {
         console.log("Gone to root");
     },
@@ -8,11 +22,18 @@ var nav = {
     }
 }
 
+/**
+ * Initialisation stuff
+ */
 $(document).ready(function() {
     
     $.rpc('get_user_details', {
         success : function( data ) {
             $('#login_area').html(data.username);
+        },
+        
+        error : function ( error ) {
+            window.location.replace("/login");
         }
     });
     
